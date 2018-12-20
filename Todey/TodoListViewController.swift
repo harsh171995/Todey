6495a280a5e99345a20ede2fab153c66ffbd45e9
Harsh Khetarpal <hkhetarpal1@gmail.com>
2018-12-20 18:11:46 +0530

    Initial Commit

---
+++ Todey/TodoListViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  Todey
+//
+//  Created by Harsh Khetarpal on 20/12/18.
+//  Copyright © 2018 Harsh Khetarpal. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    var todolist = ["Buy Apple", "Buy iPhone XR", "Buy Rebook Shose", "Complete iOS Development Course"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //MARK - Table view datasource methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todolist.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todeycell", for: indexPath)
+        
+        cell.textLabel?.text = todolist[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    // MARK - TableView Delegate Method
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+    }
+    
+    
+    // MARK - Bar Button 
+    
+    @IBAction func addNewItem(_ sender: UIBarButtonItem) {
+        
+        
+    }
+    
+
+
+}
+
